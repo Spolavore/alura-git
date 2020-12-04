5e57927b9a158dedc2274dbcaded9f7fbb31be07
João Vitor do Amaral Spolavore <j.vitor.spolavore@gmail.com>
2020-12-04 16:57:54 -0300

    adicionei os arquivo que anteriormente eu tinha apagado sem querer

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,32 +1,25 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
-botaoAdicionar.addEventListener("click" , function(){	
-	var xhr = new XMLHttpRequest(); // objeto do js responsavel de fazer requisicoes http
+botaoAdicionar.addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
 
-	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes"); //configura qual tipo de requisicao queremos (nesse caso GET que é para acessar alguma página)e onde acha-la (link)
-	
-	xhr.addEventListener("load", function(){ //evento especialista em escutar se a respota ja foi carregada
-		
-		var mensagemDeErro = document.querySelector("#erro-ajax")
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
-		if ( xhr.status == 200) {
-		mensagemDeErro.classList.add("invisivel");
-		var resposta = xhr.responseText; // conteudo da página requisitada
-		var pacientes = JSON.parse(resposta); // traduz para objetos do js
-		
-		pacientes.forEach( function(paciente) {
-			adicionaPacienteNaTabela(paciente);
-			});
-		}else {
-			
-			mensagemDeErro.classList.remove("invisivel");
-			console.log(xhr.status);
-			console.log(xhr.responseText);
-		}
-	});
+    xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
 
-	xhr.send(); //pega a requisicao que acabamos de criar e envia ela
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+    });
 
+    xhr.send();
 });
-
--- js/form.js
+++ js/form.js
@@ -1,119 +1,106 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){    //--> passamos event como parâmetro da função. Com isso, incluímos um parâmetro que está sempre presente nas funções executadas quando ocorre um evento.
-	event.preventDefault();                       //--> desativa o comportamento de recarregar a pagina para um de escrever no cosole log
-	
-	var form = document.querySelector("#form-adiciona");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
 
-	var paciente = obtemPacienteDoFormulario(form);
+    var form = document.querySelector("#form-adiciona");
 
-	
-	
-	var erros = validaPaciente(paciente);
+    var paciente = obtemPacienteDoFormulario(form);
 
-	console.log(erros);
-	if(erros.length > 0) {
-		exibeMensagensDeErro(erros);
-		return;
-	}
+    var erros = validaPaciente(paciente);
 
-	// adicionando o paciente na tabela
-	adicionaPacienteNaTabela(paciente);
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
 
-	form.reset(); //---> limpa os campos do form após adicionar um paciente
-	var mensagensErro = document.querySelector("#mensagens-erro");
-	mensagensErro.innerHTML = "";
+        return;
+    }
+
+    adicionaPacienteNaTabela(paciente);
+
+    form.reset();
+
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+
 });
 
+function obtemPacienteDoFormulario(form) {
 
-function adicionaPacienteNaTabela(paciente) {
-	var pacienteTr = montaTr(paciente);
-	var tabela = document.querySelector("#tabela-pacientes");
-	tabela.appendChild(pacienteTr);
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
 }
 
+function montaTr(paciente) {
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
-function exibeMensagensDeErro(erros){
-	var ul = document.querySelector("#mensagens-erro");
-	ul.innerHTML = ""; //---> permite controlar o html interno de um elemento
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
-	erros.forEach(function(erro) { /// ------> "para cada item do meu array , faca alguma coisa"
-		var li = document.createElement("li");
-		li.textContent = erro;
-		ul.appendChild(li);
-	})
-
+    return pacienteTr;
 }
 
-function obtemPacienteDoFormulario(form){
+function montaTd(dado, classe) {
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
 
-	var paciente = {                        //--> cria um objeto dentro do js , onde nome : (":" significa vale dentro do mundo dos objetos) form.nome.value
-		nome: form.nome.value,                                                          //semelhante a var nome = form.nome.value               
-		peso: form.peso.value,
-		altura: form.altura.value,
-		gordura: form.gordura.value,
-		imc: calculaImc(form.peso.value, form.altura.value)
-	}
-
-	return paciente;
-
-
-}
-
-function montaTr(paciente){
-	var pacienteTr = document.createElement("tr"); //--> creatElement cria um elemento deseja passado pelos ()
-	pacienteTr.classList.add("paciente");
-
-	var nomeTd = montaTd(paciente.nome, "info-nome");
-	var pesoTd = montaTd(paciente.peso,"info-peso");
-	var alturaTd = montaTd(paciente.altura,"info-altura");
-	var gorduraTd = montaTd(paciente.gordura,"info-gordura");
-	var imcTd = montaTd(paciente.imc,"info-imc");
-
-	pacienteTr.appendChild(nomeTd);         //---> utilizada para colocar um td dentro de um tr no js
-	pacienteTr.appendChild(pesoTd);
-	pacienteTr.appendChild(alturaTd);
-	pacienteTr.appendChild(gorduraTd);
-	pacienteTr.appendChild(imcTd);
-
-	return pacienteTr;
-
-
-}
-
-function montaTd(dado,classe) {
-	var td = document.createElement("td");
-	td.textContent = dado;
-	td.classList.add(classe);
-
-	return td;
+    return td;
 }
 
 function validaPaciente(paciente) {
 
-	var erros = [];
+    var erros = [];
 
-	if( paciente.nome.length == 0) {
-		erros.push("O nome não pode ser em branco");
-	}
+    if (paciente.nome.length == 0) {
+        erros.push("O nome não pode ser em branco");
+    }
 
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
 
-	if( paciente.gordura.length == 0) {
-		erros.push("A gordura não pode ser em branco");
-	}
-	if( paciente.altura.length == 0) {
-		erros.push("A altura não pode ser em branco");
-	}
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
 
-	if( paciente.peso.length == 0) {
-		erros.push("A peso não pode ser em branco");
-	}
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
 
-	if(!validaPeso(paciente.peso)) {
-		erros.push("Peso é invalido"); //---> array.push coloca a mensagem de erro dentro do nosso array
-	} 
-	if(!validaAltura(paciente.altura)) {
-		erros.push("Altura é inválida!");
-	}
-	return erros;
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
+    }
+
+    return erros;
 }
 
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
